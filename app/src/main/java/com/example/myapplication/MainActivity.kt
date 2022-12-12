6c34aa4e4e83936b09a836bbfa9a0ec7bbf4278e
sztoslol <kubuszeski05@wp.pl>
2022-12-12 14:21:10 +0100

    Poczatek-Koniec

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.CalendarView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -18,11 +19,21 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<TextView>(R.id.textView).text=currentDate
 
-        findViewById<CalendarView>(R.id.calendarView).setOnDateChangeListener { calendarView, dzien, miesiac, rok ->
-            var dat=(dzien.toString()+"-"+miesiac.toString()+"-"+rok.toString())
-            findViewById<TextView>(R.id.textView).text=dat
+
+
+            findViewById<CalendarView>(R.id.calendarView).setOnDateChangeListener { calendarView, dzien, miesiac, rok ->
+                findViewById<Button>(R.id.poczatek)
+                val dat1=(rok.toString()+"-"+(miesiac+1).toString()+"-"+dzien.toString())
+                findViewById<TextView>(R.id.textView).text=dat1
+                findViewById<Button>(R.id.poczatek).setOnClickListener {
+                    val dat2=(rok.toString()+"-"+(miesiac+1).toString()+"-"+dzien.toString())
+                    findViewById<TextView>(R.id.wyjazd).text=dat2
+                }
+                findViewById<Button>(R.id.koniec).setOnClickListener {
+                    val dat3=(rok.toString()+"-"+(miesiac+1).toString()+"-"+dzien.toString())
+                    findViewById<TextView>(R.id.przyjazd).text=dat3
+                }
+
         }
-
-
-    }
+   }
 }
